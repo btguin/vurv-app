0984147e9fe53112fd685ae8aad771fee0fadd8e
btguin <btguin@gmail.com>
2022-12-27 01:28:49 -0800

    yes

--- pages/index.js
+++ pages/index.js
@@ -51,11 +51,11 @@ export default function Index() {
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>{error.message}</div>;
+    if (!user) return <div><a href="/api/auth/login">Login</a></div>;
   
     return (
         user && (
             <div>
-            <a href="/api/auth/login">Login</a>
             <a href="/api/auth/logout">Logout</a>
               <img src={user.picture} alt={user.name} />
               <h2>{user.name}</h2>
