533e49a801e8e3fdb5f32c3b43ecfe8b3446ef38
btguin <btguin@gmail.com>
2022-12-27 13:47:36 -0800

    testssss

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,12 @@ import { Auth0Client } from "@auth0/auth0-spa-js";
 
 
 async function generateCompletions() {
-    const apiKey = process.env.OPENAI_API_KEY;
-    console.log(process.env);
+    console.log(process.env.OPENAI_API_KEY);
     const response = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       },
       body: JSON.stringify({
         model: 'text-davinci-003',
