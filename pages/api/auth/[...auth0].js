435fe6aff740d1cde5a1bf809a287aacee14f9f7
btguin <btguin@gmail.com>
2022-12-26 18:47:23 -0800

    let's try this

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,16 +1,16 @@
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0'
 import jwt from 'jsonwebtoken'
 
-const afterCallback = async (req, res, session) => {
-  const payload = {
-    userId: session.user.sub,
-    exp: Math.floor(Date.now() / 1000) + 60 * 60,
-  }
+// const afterCallback = async (req, res, session) => {
+//   const payload = {
+//     userId: session.user.sub,
+//     exp: Math.floor(Date.now() / 1000) + 60 * 60,
+//   }
 
-  session.user.accessToken = jwt.sign(payload, process.env.SUPABASE_JWT_SECRET)
+//   session.user.accessToken = jwt.sign(payload, process.env.SUPABASE_JWT_SECRET)
 
-  return session
-}
+//   return session
+// }
 
 export default handleAuth({
   async callback(req, res) {
