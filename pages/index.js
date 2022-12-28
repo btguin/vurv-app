4a441e892b8a65e99dd928ac5a783730bc0f0365
btguin <btguin@gmail.com>
2022-12-27 18:26:40 -0800

    CMON NOW

--- pages/index.js
+++ pages/index.js
@@ -30,12 +30,12 @@ import { FormControlUnstyledContext } from "@mui/base";
 // const response = await openai.listEngines();
 
 async function generateCompletions() {
-  console.log(process.env.REACT_APP_OPENAI_API_KEY)
+  console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY);
   const response = await fetch("https://api.openai.com/v1/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
     },
     body: JSON.stringify({
       model: "text-davinci-003",
