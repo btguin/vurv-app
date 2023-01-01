379eb4d885dfbb497aab4ee9c6202fe9b5a0877b
btguin <btguin@gmail.com>
2022-12-31 17:44:21 -0800

    test

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
-
-// module.exports = {
-//   webpack: (config, { isServer }) => {
-//     // Fixes npm packages that depend on `fs` module
-//     if (!isServer) {
-//       config.node = {
-//         fs: 'empty'
-//       }
-//     }
-
-//     return config
-//   }
-// }
-
-module.exports = {
-  // webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
+module.exports = nextConfig
