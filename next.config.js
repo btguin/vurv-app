249269e4adba2efa79068facbe42bf4af0ec92e4
btguin <btguin@gmail.com>
2022-12-31 17:38:16 -0800

    webpack 5

--- next.config.js
+++ next.config.js
@@ -5,15 +5,24 @@
 
 // module.exports = nextConfig
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     // Fixes npm packages that depend on `fs` module
+//     if (!isServer) {
+//       config.node = {
+//         fs: 'empty'
+//       }
+//     }
 
-    return config
-  }
-}
+//     return config
+//   }
+// }
+
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};
