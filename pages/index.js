5cbae7d302f0be3779370473ab145eee3b148467
btguin <btguin@gmail.com>
2022-12-31 19:12:50 -0800

    broke

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,8 @@ import fetch from "isomorphic-unfetch";
 import { Auth0Client } from "@auth0/auth0-spa-js";
 import { FormControlUnstyledContext } from "@mui/base";
 import { ChatGPTAPIBrowser } from "chatgpt";
+import fs from 'fs';
+
 
 // const configuration = new Configuration({
 //     organization: "org-scajRwEd9QcqjN9BrvZ2Mvv1",
